a52e69810454f0f7b32895684178690b2248c43c
Jastxz <javicraft14@gmail.com>
2026-01-11 19:50:55 +0100

    chore: Bump project version to 1.0.2, disable CORS via application properties, and explicitly define Spring Boot component scan package.

--- src/main/java/org/javig/micro_adversarial_search/MicroAdversarialSearchApplication.java
+++ src/main/java/org/javig/micro_adversarial_search/MicroAdversarialSearchApplication.java
@@ -3,7 +3,7 @@ package org.javig.micro_adversarial_search;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.javig.micro_adversarial_search")
 public class MicroAdversarialSearchApplication {
 
 	public static void main(String[] args) {
